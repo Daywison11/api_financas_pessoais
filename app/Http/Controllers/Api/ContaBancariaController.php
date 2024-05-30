64bb7a02848fb8b4c8614a52682427f8eea1947f
Daywison_Oliveira <daywison@unsoft.com.br>
2024-05-30 17:16:46 -0300

    Adicionando validações e conversão das resposta para pt_br

--- app/Http/Controllers/Api/ContaBancariaController.php
+++ app/Http/Controllers/Api/ContaBancariaController.php
@@ -43,27 +43,36 @@ class ContaBancariaController extends Controller
      */
     public function store(Request $request)
     {
+
+        $data = $request->validate([
+            'tipo_conta' => 'required|string|max:255',
+            'banco' => 'required|string|max:255',
+            'numero_conta' => 'required|string|max:255',
+            'saldo' => 'required|numeric',
+        ]);
+
+
         $user_id = $request->user()->id;
         $data = $request->all();
         $data['user_id'] = $user_id;
 
         $existe = ContasBancarias::CodigoUsuario($user_id)->where('numero_conta', $data['numero_conta'])->exists();
-        
+
+
         // var_dump($data['numero_conta']);exit;
 
-        if($existe){
+        if ($existe) {
 
             return response()->json(['mensagem' => 'Número da conta já cadastrado'], 409);
         }
 
-        if (!isset($data['saldo']) && empty($data['saldo'])){
+        if (!isset($data['saldo']) && empty($data['saldo'])) {
             $data['saldo'] = 0.00;
         }
-        
+
         $dados = ContasBancarias::create($data);
 
-        return response()->json(['mensagem' => 'Conta cadastrada com sucesso!','conta'=> $dados]);
-        
+        return response()->json(['mensagem' => 'Conta cadastrada com sucesso!', 'conta' => $dados]);
     }
 
     /**
@@ -87,30 +96,39 @@ class ContaBancariaController extends Controller
      */
     public function update(Request $request, string $id)
     {
+        
+
         $user_id = $request->user()->id;
         $data = $request->all();
         $data['user_id'] = $user_id;
-       
-       
+
+
         $conta = ContasBancarias::CodigoUsuario($user_id)->find($id);
-        
-        // return $conta ;exit;
-        if ($conta){
-                                              
-            if (!isset($data['saldo']) && empty($data['saldo'])){
+
+
+        if ($conta) {
+
+            // Verificar se o número da conta já existe para o usuário, excluindo o ID atual
+            $existe = ContasBancarias::codigoUsuario($user_id)
+                ->where('numero_conta', $data['numero_conta'])
+                ->where('id', '!=', $id)
+                ->exists();
+
+            if($existe){
+                return response()->json(['mensagem' => 'Número da conta já cadastrado em outra conta'], 409);
+            }
+
+            if (!isset($data['saldo']) && empty($data['saldo'])) {
                 $data['saldo'] = 0.00;
             }
 
-            $dados = $conta->update($data);
-            
-            
-            return response()->json(['mensagem' => 'Conta atualizada com sucesso!','conta'=>$dados]);
+            $conta->update($data);
 
-        }
-        else{
+
+            return response()->json(['mensagem' => 'Conta atualizada com sucesso!', 'conta' => $conta]);
+        } else {
             return response()->json(['mensagem' => 'Conta não encontrada'], 404);
         }
-        
     }
 
     /**
@@ -118,22 +136,20 @@ class ContaBancariaController extends Controller
      */
     public function destroy(Request $request, string $id)
     {
-        
+
         $user_id = $request->user()->id;
         $data = $request->all();
         $data['user_id'] = $user_id;
-       
-       
-        $conta = ContasBancarias::CodigoUsuario($user_id)->find($id);
-                
-        if ($conta){
-            
-            $dados = $conta->delete($conta);
-            
-            return response()->json(['mensagem' => 'Conta deletada com sucesso!','conta'=> $conta]);
 
-        }
-        else{
+
+        $conta = ContasBancarias::CodigoUsuario($user_id)->find($id);
+
+        if ($conta) {
+
+            $dados = $conta->delete($conta);
+
+            return response()->json(['mensagem' => 'Conta deletada com sucesso!', 'conta' => $conta]);
+        } else {
             return response()->json(['mensagem' => 'Conta não encontrada'], 404);
         }
     }
